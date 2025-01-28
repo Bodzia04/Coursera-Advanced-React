78219f5cb84fd4861957c3db86217d882e8ce732
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-01-28 21:24:16 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,72 +1,13 @@
 import './App.css';
 import { createContext, useState, useContext } from 'react';
 
-const ThemeContext = createContext();
-
-function ThemeProvider({children}){
-  const [theme, setTheme] = useState('light');  
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
-  }
-  
-  return(
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-      {children}
-    </ThemeContext.Provider>
-  )
-}
-
-function useTheme() {
-  return useContext(ThemeContext);
-}
-
-function ThemeToggleButton(){
-  const {theme, toggleTheme} = useTheme();
-
-  return(
-    <button
-      onClick={toggleTheme}
-      style={{
-        background: theme === 'light' ? '#fff' : '#333',
-        color: theme === 'light' ? '#000' : '#fff',
-        border: 'none',
-        padding: "10px 20px",
-        borderRadius: "5px",
-        cursor: "pointer",
-      }}
-    >
-      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
-    </button>
-  )
-}
 function App() {
-  const {theme} = useTheme();
-
+  const name = 'Bohdan';
+  const age = 20;
   return(
-    <div
-      style={{
-        background: theme === 'light' ? '#f0f0f0' : '#121212',
-        color: theme === 'light' ? '#000' : '#fff',
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</h1>
-      <ThemeToggleButton />
-    </div>
+    <ul>
+      <li>{`${name} - ${age} year`}</li>
+    </ul>
   )
 }
-
-function Root(){
-  return(
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  )
-}
-
-export default Root;
+export default App;
