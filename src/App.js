77c194b8daec79dc10bb9760fa43fc79694bb18c
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-01-30 18:25:23 +0100

    Week 2 -> 1) React Hooks and Custom Hooks -> 1) Getting started with hooks -> 5) Exercise Managing state within a component (задачка працює, але може відрізнятись код від коду coursera, тому що немав доступ до відповіді задачкі)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import './App.css';
-import { createContext, useState, useContext } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [greeting, setGreeting] = useState(
-    {
-    greet: 'Hello',
-    place: 'world'
-    }
-  )
-  
-  function updateGreeting(){
-    setGreeting(prevState => {
-      return {...prevState, place: 'Bohdan'}
-    })
-  }
-  return(
-    <>
-    <h1>{greeting.greet}, {greeting.place}</h1>
-    <button onClick={updateGreeting}>Update greeting</button>
-    </>
+  const [giftCard, setGiftCard] = useState({
+    customer: 'Bohdan Marchuk',
+    text: 'Free dinner for 4 guests.',
+    instructions: 'To use your coupon, click the button below.',
+    valid: true
+  });
+
+  function spendGiftCard(){
+    setGiftCard(prevState => {
+      return ({
+        ...prevState,
+        text: 'Your coupon has been used.',
+        instructions: 'Please visit our restaurant to renew your gift card.',
+        valid: false
+      });
+    });
+  };
+
+  return (
+    <div>
+      <h1>Gift Card Page</h1>
+      <h2>Customer: {giftCard.customer}</h2>
+      <h3>{giftCard.text}</h3>
+      <p>{giftCard.instructions}</p>
+      {giftCard.valid && <button onClick={spendGiftCard}>Spend Gift Card</button>}
+    </div>
   )
 }
 export default App;
