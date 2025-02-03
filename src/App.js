b1a5c3e9d44be4b3343d030bf6a07069646f7f6e
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-02-03 18:12:38 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
-    console.log('***useEffect');
-  },[]);
+    console.log('count:', count);
+  }, [count])
+
   return(
-    <h1>Title</h1>
+    <>
+    <p>Counter: {count}</p>
+    <button onClick={() => setCount(count + 1)}>Enlarge</button>
+    </>
   )
 }
 export default App;
