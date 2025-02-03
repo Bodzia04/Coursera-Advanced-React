1153c38ca304ae5003e5513da1a6b6af7f831594
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-02-03 18:04:13 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [giftCard, setGiftCard] = useState({
-    customer: 'Bohdan Marchuk',
-    text: 'Free dinner for 4 guests.',
-    instructions: 'To use your coupon, click the button below.',
-    valid: true
-  });
-
-  function spendGiftCard(){
-    setGiftCard(prevState => {
-      return ({
-        ...prevState,
-        text: 'Your coupon has been used.',
-        instructions: 'Please visit our restaurant to renew your gift card.',
-        valid: false
-      });
-    });
-  };
-
-  return (
-    <div>
-      <h1>Gift Card Page</h1>
-      <h2>Customer: {giftCard.customer}</h2>
-      <h3>{giftCard.text}</h3>
-      <p>{giftCard.instructions}</p>
-      {giftCard.valid && <button onClick={spendGiftCard}>Spend Gift Card</button>}
-    </div>
+  useEffect(() => {
+    console.log('***useEffect');
+  },[]);
+  return(
+    <h1>Title</h1>
   )
 }
 export default App;
