61cf7f6ee5f151e75131e6adc0f07c09bd1329d5
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-02-06 21:59:44 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-
+import './App.css'
+function reducer(state, action){
+  if(action.type === 'decrement') return {money: state.money - 10};
+  if(action.type === 'increment') return {money: state.money + 10};
+}
 function App() {
-  const [user, setUser] = React.useState([]);
 
-  const fetchData = () => {
-    fetch('https://randomuser.me/api/?results=1')
-      .then((response) => response.json())
-      .then((data) => setUser(data))
-  };
 
-  React.useEffect(() => {
-    fetchData();
-  },[]);
-
-  return Object.keys(user).length > 0 ? (
-    <div style={{padding: '40px'}}>
-      <h1>Customer data</h1>
-      <h2>Name: {user.results[0].name.first}</h2>
-      <img src={user.results[0].picture.large} alt=''></img>
+  const initialState = {money: 100}
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return (
+    <div className='App'>
+      <h1>Wallet: {state.money}</h1>
+      <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+      <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
     </div>
-  ) : (
-    <h1>Data pending...</h1>
-  );
+  )
 }
 
 export default App
