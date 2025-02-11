d2d847ec722f835f1e385a10016e1a018012a1d5
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-02-11 16:53:52 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect} from 'react';
 
-export default function App(){
-  const [hour, setHour] = useState();
-  const intervalRef = useRef();
+function useWindowWidth(){
+  const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    startHour();
-
-    return () => clearInterval(intervalRef.current);
-  }, []);
-
-  function stopHour(){
-    clearInterval(intervalRef.current)
-  }
-
-  function startHour(){
-    intervalRef.current = setInterval(() => {
-      setHour(new Date().toLocaleTimeString());
-    }, 1000)
-
-  }
-
-  return (
-    <div>
-      <p>Hour: {hour}</p>
-      <button onClick={stopHour}>Stop</button>
-      <button onClick={startHour}>Start</button>
-    </div>
-  )
-  
-
-}
+    const handleWidth = () => {setWidth(window.innerWidth)};
+    
+    window.addEventListener('resize', handleWidth);
+    
+    return () => window.removeEventListener('resize', handleWidth);
+  }, []); 
+};
