b9f4db70554538a09d064976633015fecfea2931
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-02-12 21:48:43 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useState, useEffect} from 'react';
+function Card({title, children}){
+  return (
+    <div className='card'>
+      <h1>{title}</h1>
+      <div className='card-content'>
+        <p>{children}</p>
+      </div>
+    </div>
+  )
+};
 
-function useWindowWidth(){
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleWidth = () => {setWidth(window.innerWidth)};
-    
-    window.addEventListener('resize', handleWidth);
-    
-    return () => window.removeEventListener('resize', handleWidth);
-  }, []); 
+export default function App(){
+  return (
+    <Card title='React props'>
+      <p>Lorem nbtbnt ijtnbtbnt rbnr tbn tbntbnrbnrt ng gn ergnre gnre re   </p>
+    </Card>
+  )
 };
