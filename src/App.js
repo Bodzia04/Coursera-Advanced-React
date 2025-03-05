cb0ed595164e566eddf389a2ca90bd01b91b8978
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-03-05 06:54:29 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
-const obj = {
-  id: 1,
-  item: 'Orange'
-}
-
-const copyObj = {...obj, item: 'Apple'};
-console.log('copyObj',copyObj);
-console.log('obj', obj);
-
-
-const Row = ({children}) => {
+const User = ({name}) => {
   return(
-    <div className='Row'>
-      {React.Children.map(children, (child, index) => {
-        return child
-      })}
+    <div>
+      <h1>{name}</h1>
     </div>
   )
-};
+}
 
 export default function App(){
   return(
-    <div className='App'>
-      <Row spasing={32}>
-        <p>Bohdan</p>
-        <p>Marchuk</p>
-        <p>20 years</p>
-      </Row>
-    </div>
+    <User name='Bohdan'></User>
   )
 }
