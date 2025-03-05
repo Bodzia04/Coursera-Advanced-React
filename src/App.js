b55b27c3bc4856ef28340200a5c46f34bae41745
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-03-05 21:40:03 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-const User = ({name}) => {
+const Person1 = () => {
+  const [money, setMoney] = React.useState(10);
+
+  function handleIncrease(){
+    setMoney(money * 2);
+  }
   return(
     <div>
-      <h1>{name}</h1>
+      <h2>Bohdan has money ${money}</h2>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  )
+}
+
+const Person2 = () => {
+  return(
+    <div>
+      <h2>Vova has money $10</h2>
+      <button>Increase</button>
     </div>
   )
 }
 
 export default function App(){
   return(
-    <User name='Bohdan'></User>
+    <div>
+      <Person1/>
+      <Person2/>
+    </div>
   )
 }
