b2dbb9450172798839505e545f5028e142f7efdf
Bohdan Marchuk <bogdanmarchuk04@gmail.com>
2025-03-06 06:51:29 +0100

    aDraft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Person1 = () => {
-  const [money, setMoney] = React.useState(10);
-
-  function handleIncrease(){
-    setMoney(money * 2);
+const updatedComponent = (OriginalComponent) => {
+  function NewComponent(){
+    const [money, setMoney] = React.useState(10);
+    function handleIncrease(){
+      setMoney(money * 2);
+    }
+    return <OriginalComponent handleIncrease={handleIncrease} money={money}/>
   }
+  return NewComponent;
+}
+
+const Person1 = ({handleIncrease, money}) => {
   return(
     <div>
       <h2>Bohdan has money ${money}</h2>
@@ -14,20 +20,23 @@ const Person1 = () => {
   )
 }
 
-const Person2 = () => {
+const Person2 = ({handleIncrease, money}) => {
   return(
     <div>
-      <h2>Vova has money $10</h2>
-      <button>Increase</button>
+      <h2>Vova has money ${money}</h2>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   )
 }
 
+const EnhancedPerson1 = updatedComponent(Person1);
+const EnhancedPerson2 = updatedComponent(Person2);
+
 export default function App(){
   return(
     <div>
-      <Person1/>
-      <Person2/>
+      <EnhancedPerson1/>
+      <EnhancedPerson2/>
     </div>
   )
 }
